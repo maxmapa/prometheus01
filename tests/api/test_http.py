2113767588a566274c72a11c0ac995553a3b0f7b
maxmapa <maxmapa@gmail.com>
2024-05-17 17:25:19 +0100

    Added http test

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -10,4 +10,16 @@ def test_first_request():
 @pytest.mark.http
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
-    print(f"Response Body is {r.text}")
+    body = r.json()
+    headers = r.headers
+
+    assert body['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    assert headers['Server'] == 'GitHub.com'
+
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/sergii_butenko')
+
+    assert r.status_code == 404
