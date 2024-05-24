f8933540230ee0f69f1e16edcf35db9a5995d6cb
maxmapa <maxmapa@gmail.com>
2024-05-24 16:07:01 +0100

    more api tests

--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -16,7 +16,7 @@ def test_user_not_exists(github_api):
 @pytest.mark.api
 def test_repo_can_be_found(github_api):
     r = github_api.search_repo('become-qa-auto')
-    assert r['total_count'] == 13
+    assert r['total_count'] == 57
     assert 'become-qa-auto' in r['items'][0]['name'] 
 
 
